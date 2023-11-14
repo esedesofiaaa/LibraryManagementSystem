5c998c4a15e42cc1bd0b5725d02eb257ac6af670
esedesofiaaa <sofia.santis2@aiesec.net>
2023-11-14 07:36:50 -0500

    Se crea mockito de la interfaz LibraryDataBase y el test de LoanManager

--- src/main/java/org/example/MockableLibraryDataBase.java
+++ src/main/java/org/example/MockableLibraryDataBase.java
@@ -1,2 +1,41 @@
-package org.example;public class MockableLibraryDataBase {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+public class MockableLibraryDataBase implements LibraryDatabase {
+
+    private List<Book> libros = new ArrayList<>();
+    private List<Student> estudiantes = new ArrayList<>();
+
+
+    @Override
+    public void saveBookData(Book book) {
+        libros.add(book);
+    }
+
+    @Override
+    public void saveStudentData(Student student) {
+        estudiantes.add(student);
+    }
+
+    @Override
+    public Book getBookById(String id) {
+        for (int i = 0; i <libros.size() ; i++) {
+            if(id == libros.get(i).getId()){
+                return libros.get(i);
+            }
+        }
+        return null;
+
+    }
+
+    @Override
+    public Student getStudentById(String id) {
+        for (int i = 0; i <estudiantes.size() ; i++) {
+            if(id == estudiantes.get(i).getId()){
+                return estudiantes.get(i);
+            }
+        }
+        return null;
+    }
 }
--- src/test/java/LoanManagerTest.java
+++ src/test/java/LoanManagerTest.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class LoanManagerTest {
+import org.example.Book;
+import org.example.LoanManager;
+import org.example.Student;
+import org.example.LibraryDatabase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoanManagerTest {
+    private LoanManager sofia;
+    private Book book1;
+    private Student student;
+    private LibraryDatabase libraryDatabaseMock;
+
+    @BeforeEach
+    public void setUp() {
+        libraryDatabaseMock = mock(LibraryDatabase.class);
+        sofia = new LoanManager(libraryDatabaseMock);
+    }
+
+   @Test
+   void TestLendBook (){
+       student = new Student("123", "John Doe");
+       book1 = new Book("B001", "Effective Java", "Joshua Bloch");
+
+   }
 }
